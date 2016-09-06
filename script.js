5476b0aae0791a3821671cb51d87400e5685de4c
Yuya Matsuo <niu@niusounds.com>
2016-09-07 01:29:39 +0900

    update title

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
 var i = 100;
 
-var timerId = setInterval(function(){
-$("#aura").toggleClass("suspend");
-$("#aura").toggleClass("progress");
-$("#sight").toggleClass("turnblue");
+var timerId = setInterval(function () {
+  $("#aura").toggleClass("suspend");
+  $("#aura").toggleClass("progress");
+  $("#sight").toggleClass("turnblue");
 
-navigator.vibrate([
-  i, i,
-  i * 3, i,
-  i, i,
-  i * 5, i * 2
-]);
+  navigator.vibrate([
+    i, i,
+    i * 3, i,
+    i, i,
+    i * 5, i * 2
+  ]);
 }, i * 17);
